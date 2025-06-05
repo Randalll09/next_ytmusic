2dfebf0fee2990800f49087e18d98373c46ee6ac
Najji09 <100065117+Najji09@users.noreply.github.com>
2025-06-05 19:50:57 +0900

    7.14

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const page = (props) => {
-  console.log(props);
-  return <div>playlist {props.searchParams.list}</div>;
+const page = () => {
+  // console.log(props);
+  return <div>playlist </div>;
 };
 
 export default page;
